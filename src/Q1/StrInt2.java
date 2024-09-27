69575250f2438e785e0cc26373fd5c0e72e2e476
Zach-Ron <ZacharyRonek@gmail.com>
2024-09-27 11:15:39 -0500

    Zach-Ron

---
+++ src/Q1/StrInt2.java
@@ -0,0 +1,27 @@
+package Q1;
+import java.util.*;
+
+public class StrInt2 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String word = input.nextLine();
+        String word2 = input.nextLine();
+        word = word.toLowerCase();
+        word2 = word2.toLowerCase();
+
+        if (word.length() != word2.length()) {
+            System.out.println("not Anagrams :(");
+        } else {
+            for (int lcv = 0; lcv < word.length(); lcv++){
+                String c = word.substring(lcv, lcv+1);
+                int i = word2.indexOf(c);
+                if (i != -1){
+                    word2 = word2.substring(0, i) + word2.substring(beginIndex: i+1);
+                } else {
+                    System.out.println("Not Anagrams");
+                    break;
+                }
+            }
+        }
+    }
+}
--- src/Q1/prog_82a.java
+++ src/Q1/prog_82a.java
@@ -1,7 +1,9 @@
 package Q1;
+import java.util.Scanner;
 
 public class prog_82a {
     public static void main(String[] args){
+Scanner Input = new Scanner(System.in);
 
     }
 }
