431f7b0f1fd642b5bd742914e8927b100c3a4640
Zach-Ron <ZacharyRonek@gmail.com>
2024-12-18 11:37:05 -0600

    Zach-Ron

--- src/Q2/prog435A/CL435.java
+++ src/Q2/prog435A/CL435.java
@@ -1,30 +1,59 @@
 package Q2.prog435A;
 
 public class CL435 {
-    private int vehicleNum;
-    private double factor;
-    private double price;
-    private int gateNum;
-    private int total;
+    private double price, factor, total;
+    private int gateNum, vehicleNum;
+    String car;
+
+    public CL435(){
+        vehicleNum = 0;
+        gateNum = 0;
+        price = 0;
+        factor = 0;
+        total = 0;
+        car = "";
+    }
 
     public CL435(int number, int gate){
         vehicleNum = number;
         gateNum = gate;
-        total = 0;
     }
 
-    public double getVehicleFactor()
+    public void setVehicleFactor()
     {
-        if (vehicleNum == 1) factor = 1.00;
-        else if (vehicleNum == 2) factor = 1.30;
-        else if (vehicleNum == 3) factor = 1.60;
-        else if (vehicleNum == 4) factor = 2.00;
-        else if (vehicleNum == 5) factor = 2.40;
-        else factor = 2.70;
-        return (factor);
+        if (vehicleNum == 1)
+        {
+            factor = 1.00;
+            car = "Compact Car";
+        }
+        else if (vehicleNum == 2)
+        {
+            factor = 1.30;
+            car = "Small Car  ";
+        }
+        else if (vehicleNum == 3)
+        {
+            factor = 1.60;
+            car = "Mid Size Car";
+        }
+        else if (vehicleNum == 4)
+        {
+            factor = 2.00;
+            car = "Full Size Car";
+        }
+        else if (vehicleNum == 5)
+        {
+            factor = 2.40;
+            car = "Truck        ";
+        }
+        else
+        {
+            factor = 2.70;
+            car = "16 Wheeler   ";
+        }
     }
 
-    public double getRate()
+    public void setRate()
     {
         if (gateNum == 1) price = 1.35;
         else if (gateNum == 2) price = 2.00;
@@ -34,14 +63,24 @@ public class CL435 {
         else if (gateNum == 6) price = 4.80;
         else if (gateNum == 7) price = 5.50;
         else price = 6.00;
-
-        return (price);
     }
 
-    public double getTotal()
+    public void setTotal()
     {
-        total =
+        total = price * factor;
+    }
+
+    public double getTotal(){
         return total;
+    }
+    public double getRate(){
+        return (price);
+    }
+    public double getFactor(){
+        return(factor);
+    }
+    public String getCar(){
+        return (car);
     }
 
     }
--- src/Q2/prog435A/prog435a.java
+++ src/Q2/prog435A/prog435a.java
@@ -6,13 +6,15 @@ import java.io.IOException;
 
 public class prog435a {
     public static void main(String[] args){
+        System.out.println("Car Type\t\t\tBase Toll\t\t\tFactor\t\t\tCost");
         try{
             Scanner file = new Scanner(new File("Langdat/prog435a.DAT"));
             while (file.hasNext()){
                 int number = file.nextInt();
                 int gate = file.nextInt();
-                file.nextLine();
                 new CL435(number, gate);
+                System.out.println(cl435.getCar() + "\t\t\t" + cl435.getRate() +"\t\t\t  " + cl435.getFactor()
+                + "\t\t\t  " + cl435.getTotal());
 
 
                 //System.out.println(file.next());
