496cf2fc22da621064b4129addc24f4556cd7431
Zach-Ron <ZacharyRonek@gmail.com>
2024-12-19 11:22:44 -0600

    Zach-Ron

--- src/Q2/prog435A/CL435.java
+++ src/Q2/prog435A/CL435.java
@@ -3,7 +3,7 @@ package Q2.prog435A;
 public class CL435 {
     private double price, factor, total;
     private int gateNum, vehicleNum;
-    String car;
+    String car, strPrice;
 
     public CL435(){
         vehicleNum = 0;
@@ -12,6 +12,7 @@ public class CL435 {
         factor = 0;
         total = 0;
         car = "";
+        strPrice = "";
     }
 
     public CL435(int number, int gate){
@@ -24,12 +25,12 @@ public class CL435 {
         if (vehicleNum == 1)
         {
             factor = 1.00;
-            car = "Compact Car";
+            car = "Compact Car\t";
         }
         else if (vehicleNum == 2)
         {
             factor = 1.30;
-            car = "Small Car  ";
+            car = "Small Car  \t";
         }
         else if (vehicleNum == 3)
         {
@@ -44,12 +45,12 @@ public class CL435 {
         else if (vehicleNum == 5)
         {
             factor = 2.40;
-            car = "Truck        ";
+            car = "Truck\t\t";
         }
         else
         {
             factor = 2.70;
-            car = "16 Wheeler   ";
+            car = "16 Wheeler\t";
         }
     }
 
@@ -63,18 +64,26 @@ public class CL435 {
         else if (gateNum == 6) price = 4.80;
         else if (gateNum == 7) price = 5.50;
         else price = 6.00;
+
+        price = price * 100;
+        price = Math.round(price);
+        price /= 100;
+        strPrice = String.format("%.2f", price);
     }
 
     public void setTotal()
     {
         total = price * factor;
+        total = total * 100;
+        total = Math.round(total);
+        total /= 100;
     }
 
     public double getTotal(){
         return total;
     }
-    public double getRate(){
-        return (price);
+    public String getRate(){
+        return (strPrice);
     }
     public double getFactor(){
         return(factor);
--- src/Q2/prog435A/prog435a.java
+++ src/Q2/prog435A/prog435a.java
@@ -6,15 +6,20 @@ import java.io.IOException;
 
 public class prog435a {
     public static void main(String[] args){
+
         System.out.println("Car Type\t\t\tBase Toll\t\t\tFactor\t\t\tCost");
+        System.out.println();
         try{
             Scanner file = new Scanner(new File("Langdat/prog435a.DAT"));
             while (file.hasNext()){
                 int number = file.nextInt();
                 int gate = file.nextInt();
-                new CL435(number, gate);
-                System.out.println(cl435.getCar() + "\t\t\t" + cl435.getRate() +"\t\t\t  " + cl435.getFactor()
-                + "\t\t\t  " + cl435.getTotal());
+                CL435 cl435 = new CL435(number, gate);
+                cl435.setRate();
+                cl435.setVehicleFactor();
+                cl435.setTotal();
+                System.out.println(cl435.getCar() + "\t\t" + cl435.getRate() +"\t\t\t\t" + cl435.getFactor()
+                + "\t\t\t\t" + cl435.getTotal());
 
 
                 //System.out.println(file.next());
@@ -27,3 +32,27 @@ public class prog435a {
 
     }
 }
+/*
+Car Type			Base Toll			Factor			Cost
+
+Compact Car			1.35				1.0				1.35
+Small Car  			2.50				1.3				3.25
+Mid Size Car		4.10				1.6				6.56
+Full Size Car		5.50				2.0				11.0
+Truck				2.00				2.4				4.8
+16 Wheeler			3.25				2.7				8.78
+Compact Car			4.80				1.0				4.8
+Small Car  			6.00				1.3				7.8
+Mid Size Car		1.35				1.6				2.16
+Full Size Car		2.50				2.0				5.0
+Truck				4.10				2.4				9.84
+16 Wheeler			5.50				2.7				14.85
+Compact Car			6.00				1.0				6.0
+Small Car  			1.35				1.3				1.76
+Mid Size Car		2.00				1.6				3.2
+Full Size Car		2.50				2.0				5.0
+Truck				3.25				2.4				7.8
+16 Wheeler			4.10				2.7				11.07
+
+Process finished with exit code 0
+ */
