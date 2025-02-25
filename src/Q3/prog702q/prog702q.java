cba7905cb3de0384054b83a342e52f7b07010745
Zach-Ron <ZacharyRonek@gmail.com>
2025-02-25 11:32:23 -0600

    Zach-Ron

--- src/Q3/prog702q/prog702q.java
+++ src/Q3/prog702q/prog702q.java
@@ -3,12 +3,17 @@ package Q3.prog702q;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class prog702q {
     public static void main(String[] args){
         try {
             Scanner file = new Scanner(new File("Langdat/prog702q.txt"));
+            List<vehicle> tech = new ArrayList<>();
 
+            int h = file.nextInt();
+            if ()
 
         } catch(IOException e)
         {
