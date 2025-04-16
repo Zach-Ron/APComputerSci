18d7f4125798b23fafbd829db6d80ce5980193d4
Zach-Ron <ZacharyRonek@gmail.com>
2025-04-16 11:34:46 -0500

    Zach-Ron

--- src/Q3/LibraryManagement/BinarySearchUtil.java
+++ src/Q3/LibraryManagement/BinarySearchUtil.java
@@ -5,17 +5,20 @@ import java.util.ArrayList;
 public class BinarySearchUtil {
     // TODO: Implement binary search method (takes in ArrayList) using recursion for searching books by title
     //  (required) or author
-    public ArrayList sortByTitle(ArrayList original)
+    public ArrayList sortByTitle(ArrayList<String> original)
     {
         ArrayList<String> OG = original;
         ArrayList sortedTitle = new ArrayList<String>();
-        String current = (String) original.;
+        String current = original.get(0);
         while (sortedTitle.size() > original.size())
         {
-            for (int lcv = 0; lcv < OG.size(); lcv++)
+            for (int lcv = 1; lcv < OG.size(); lcv++)
             {
-                if (current.compareToIgnoreCase(OG.get(0)))
-                current =
+                lcv--;
+                if (current.compareToIgnoreCase(OG.get(lcv)) >= 0)
+                current = OG.get(lcv);
+                OG.remove(lcv);
+                sortedTitle.add(0, current);
             }
         }
         return sortedTitle;
