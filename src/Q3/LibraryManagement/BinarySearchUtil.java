ddd94eb44b7ddc2de27922f04a56dd72199284b2
Zach-Ron <ZacharyRonek@gmail.com>
2025-04-23 11:46:30 -0500

    Zach-Ron

--- src/Q3/LibraryManagement/BinarySearchUtil.java
+++ src/Q3/LibraryManagement/BinarySearchUtil.java
@@ -9,28 +9,36 @@ public class BinarySearchUtil {
         ArrayList<String> copyOG = original;
         ArrayList sortedTitle = new ArrayList<String>();
         String current = original.get(0); //current is current largest string value
+        String smallest = original.get(0);//smallest string val
         sortedTitle.add(current);
         while (sortedTitle.size() > original.size())
         {
-             byte loop = 0;
+             byte loop = 1;
             for (int lcv = 1; lcv < copyOG.size(); lcv++)
             {
-                if (current.compareToIgnoreCase(copyOG.get(lcv)) == -1 && current.compareToIgnoreCase(copyOG.get(lcv)) !=)
+                if (current.compareToIgnoreCase(copyOG.get(lcv)) == -loop)
                 {
                     current = copyOG.get(lcv);
                     copyOG.remove(lcv);
                     sortedTitle.add(0, current);
-                    lcv--;
-                }
-                else if (loop > 0 && current.compareToIgnoreCase(copyOG.get(lcv)) == 1)
-                {
 
+                }
+                else if (smallest.compareToIgnoreCase(copyOG.get(lcv)) == loop)
+                {
+                    smallest = copyOG.get(lcv);
+                    copyOG.remove(lcv);
+                    sortedTitle.add(smallest);
+                    lcv--;
                 }
             }
             loop++;
         }
         return sortedTitle;
+
     }
+
+
+    public
     //Note: if .compareToIgnoreCase returns num < 0 then the parameter sent in comes first and vice versa.
     //Check for numbers of 1, -1, or 0.
 
@@ -39,10 +47,10 @@ public class BinarySearchUtil {
 
 
     // TODO: complete sorting method based on alphabetical order of author names
-    public ArrayList sortByAuthor(ArrayList<String> original){
-        ArrayList<String> copyOG = original;
+    public ArrayList sortByAuthor(ArrayList<Book> original){
+        ArrayList<Book> copyOG = original;
         ArrayList sortedAuthor = new ArrayList<String>();
-        String current = original.get(0);
+        //String current = original.get(0);
 
         return sortedAuthor;
     }
