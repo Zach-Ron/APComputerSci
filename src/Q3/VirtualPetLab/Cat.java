de5596ebccbd2dcbbe0467d0c2120dcfaafa82d7
Zach-Ron <ZacharyRonek@gmail.com>
2025-05-12 21:47:30 -0500

    Enhance feeding, playing, and sleeping behaviors for Cat and Dog classes by fixing attribute clamping

--- src/Q3/VirtualPetLab/Cat.java
+++ src/Q3/VirtualPetLab/Cat.java
@@ -9,15 +9,27 @@ public class Cat extends Pet {
     @Override
     public void feed() {
         // TODO: Implement feeding behavior for Cat
+        super.setHunger((int) (super.getHunger() - ((Math.random() * 8) + 9)));
+        super.setEnergy((int) (super.getEnergy() + ((Math.random() * 15) + 10)));
+        super.setHappiness((int) (super.getHappiness() + ((Math.random() * 5) + 10)));
+        super.clampAttributes();
     }
 
     @Override
     public void play() {
         // TODO: Implement playing behavior for Cat
+        super.setEnergy((int) (super.getEnergy() - ((Math.random() * 10) + 5)));
+        super.setHunger((int) (super.getHunger() + ((Math.random() * 7) + 5)));
+        super.setHappiness((int) (super.getHappiness() + ((Math.random() * 5) + 10)));
+        super.clampAttributes();
     }
 
     @Override
     public void sleep() {
         // TODO: Implement sleeping behavior for Cat
+        super.setEnergy((int) (super.getEnergy() + ((Math.random() * 10) + 5)));
+        super.setHunger((int) (super.getHunger() + ((Math.random() * 7) + 5)));
+        super.setHappiness((int) (super.getHappiness() + ((Math.random() * 5) + 10)));
+        super.clampAttributes();
     }
 }
